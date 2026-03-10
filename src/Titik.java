9db0b52ce5f06eda33cad244e167c8dd12e1c85d
AlfonsoHutahaean <alfonsohutahaeann@gmail.com>
2026-03-10 11:16:04 +0700

    menambahkan class Titik

---
+++ src/Titik.java
@@ -0,0 +1,34 @@
+public class Titik {
+    double absis;
+    double ordinat;
+
+    Titik() {
+        absis = 0;
+        ordinat = 0;
+    }
+
+    double getAbsis() {
+        return absis;
+    }
+
+    double getOrdinat() {
+        return ordinat;
+    }
+
+    void setAbsis(double x) {
+        absis = x;
+    }
+
+    void setOrdinat(double y) {
+        ordinat = y;
+    }
+
+    void geser(double x, double y) {
+        absis += x;
+        ordinat += y;
+    }
+
+    void printTitik() {
+        System.out.println("(" + absis + "," + ordinat + ")");
+    }
+}
