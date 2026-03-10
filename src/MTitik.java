500036a02160f82c966386ce3457956df81d2bc7
AlfonsoHutahaean <alfonsohutahaeann@gmail.com>
2026-03-10 11:23:25 +0700

    revisi pertemuan 1

--- src/MTitik.java
+++ src/MTitik.java
@@ -1,14 +1,27 @@
+/*
+ * Nama File    : MTitik.java
+ * Deskripsi    : Main class untuk menguji class Titik
+ * Pembuat      : Alfonso Michael Normando Hutahaean
+ * Tanggal      : 10 Maret 2026
+ */
+
 public class MTitik {
     public static void main(String[] args) {
+        
+        // Membuat objek titik
         Titik t1 = new Titik();
 
+        // Mengatur koordinat titik
         t1.setAbsis(3);
         t1.setOrdinat(4);
 
+        // Menampilkan titik awal
         t1.printTitik();
 
+        // Menggeser titik
         t1.geser(2, 1);
 
+        // Menampilkan titik setelah digeser
         t1.printTitik();
     }
 }
--- src/Titik.java
+++ src/Titik.java
@@ -1,33 +1,47 @@
+/*
+ * Nama File    : Titik.java
+ * Deskripsi    : Class Titik dengan atribut absis dan ordinat
+ * Pembuat      : Alfonso Michael Normando Hutahaean
+ * Tanggal      : 10 Maret 2026
+ */
+
 public class Titik {
     double absis;
     double ordinat;
 
+    // Konstruktor
     Titik() {
-        absis = 0;
-        ordinat = 0;
+        absis = 0.0;
+        ordinat = 0.0;
     }
 
+    // Mengambil nilai absis
     double getAbsis() {
         return absis;
     }
 
+    // Mengambil nilai ordinat
     double getOrdinat() {
         return ordinat;
     }
 
+    // Mengeset nilai absis
     void setAbsis(double x) {
         absis = x;
     }
 
+    // Mengeset nilai ordinat
     void setOrdinat(double y) {
         ordinat = y;
     }
 
+    // Menggeser titik
     void geser(double x, double y) {
-        absis += x;
-        ordinat += y;
+        absis = absis + x;
+        ordinat = ordinat + y;
     }
 
+    // Menampilkan koordinat titik
     void printTitik() {
         System.out.println("(" + absis + "," + ordinat + ")");
     }
