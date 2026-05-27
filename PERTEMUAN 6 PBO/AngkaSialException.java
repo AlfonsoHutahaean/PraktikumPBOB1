8ef4ea48677f399ca8f9848b98d0ffabad638675
AlfonsoHutahaean <163295212+AlfonsoHutahaean@users.noreply.github.com>
2026-05-28 01:22:38 +0700

    Add files via upload

---
+++ PERTEMUAN 6 PBO/AngkaSialException.java
@@ -0,0 +1,9 @@
+public class AngkaSialException
+        extends Exception {
+
+    public AngkaSialException() {
+
+        super(
+                "jangan memasukkan angka 13 karena angka sial!!!");
+    }
+}
